bf5830c7f2b95ab9a401af2d8ab305d9f78ce531
kristina.sigal <kulrikh@gmail.com>
2022-08-15 01:44:03 +0300

    Add commit 10,13,15

---
+++ Task10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая 
+//      1. принимает на вход трёхзначное число и 
+//      2. на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число.");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int MidleDigit(int num)
+{
+    int midleDigit = (num / 10) % 10;
+    return midleDigit > 0 ? midleDigit : midleDigit * -1;
+}
+
+int res = MidleDigit(number);
+Console.WriteLine(res);
---
+++ Task15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 15: Напишите программу, которая 
+//         1. принимает на вход цифру, обозначающую день недели, и 
+//         2. проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите цифру от 1 до 7.");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool CheckDay(int num)
+{
+    return num == 6 || num == 7;
+}
+
+bool result = CheckDay(number);
+Console.WriteLine(number > 7 || number < 1 ? "Ошибка ввода." : (result ? "Да" : "Нет"));
